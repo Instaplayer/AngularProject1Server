87f646507f08a5f4135175113f6c89871b2cfacc
DOVER-GLOBAL\10083063 <c-jproszek@doverfs.com>
2021-12-17 10:50:44 +0100

    Koniec Praktyk Submit :3

---
+++ WebApplication1/Controllers/ngrxController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    [Route("/ngrx")]
+    [ApiController]
+
+    public class ngrxController : Controller
+    {
+        public static List<Products> ProductsDB = new List<Products>();
+
+        [HttpGet(Name = "getProducts")]
+        public IEnumerable<Products> getProducts(bool getSpecific, int? id)
+        {
+
+            if (getSpecific == true)
+            {
+                return ProductsDB.Where(c => c.id == id).ToArray();
+            }
+            else
+            {
+                return ProductsDB.Where(c => c.id != null).ToArray();
+            }
+            
+        }
+
+        [HttpPost(Name = "postProducts")]
+        public Products postProducts(Products product)
+        {
+            product.id = ProductsDB.Where((c) => c.id != null).Count() + 1;
+            ProductsDB.Add(product);
+
+            return product;
+        }
+
+        [HttpDelete(Name = "delProducts")]
+        public Products delProducts(int index)
+        {
+            Products del = ProductsDB[index];
+
+            if(del != null) { 
+                ProductsDB.Remove(del);
+            }
+
+            return del;
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/ngrx.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.ViewModels
+{
+    public class Products
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int price { get; set; }
+
+        public int count { get; set; }
+    }
+
+}
+
